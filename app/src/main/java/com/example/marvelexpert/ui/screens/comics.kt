e4a98544615850006ef3576cf5db24e9ccc8b037
Eugenio <eormartinez20@gmail.com>
2023-01-10 23:51:09 -0400

    Se agrega el TabRow

--- app/src/main/java/com/example/marvelexpert/ui/screens/comics.kt
+++ app/src/main/java/com/example/marvelexpert/ui/screens/comics.kt
@@ -1,8 +1,8 @@
 package com.example.marvelexpert.ui.screens
 
 import androidx.compose.foundation.ExperimentalFoundationApi
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import coil.annotation.ExperimentalCoilApi
@@ -10,8 +10,8 @@ import com.example.marvelexpert.data.entities.Comic
 import com.example.marvelexpert.data.repositories.ComicsRepository
 import com.example.marvelexpert.ui.screens.common.MarvelItemDetailScreen
 import com.example.marvelexpert.ui.screens.common.MarvelItemList
-import com.google.accompanist.pager.ExperimentalPagerApi
-import com.google.accompanist.pager.HorizontalPager
+import com.google.accompanist.pager.*
+import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalPagerApi::class)
 @ExperimentalCoilApi
@@ -22,14 +22,49 @@ fun ComicsScreen(onClick: (Comic) -> Unit) {
     LaunchedEffect(Unit) {
         comicsState = ComicsRepository.get()
     }
-    val formats = Comic.Format.values()
-    HorizontalPager(
-        count = formats.size
+    val formats = Comic.Format.values().take(3)
+    val pagerState = rememberPagerState()
+
+
+    Column {
+        ComicFormatsTabRow(pagerState, formats)
+        HorizontalPager(
+            count = formats.size,
+            state = pagerState
+        ) {
+            MarvelItemList(
+                items = comicsState,
+                onClick = onClick
+            )
+        }
+    }
+
+}
+
+@OptIn(ExperimentalPagerApi::class)
+@Composable
+private fun ComicFormatsTabRow(
+    pagerState: PagerState,
+    formats: List<Comic.Format>
+) {
+    val scope = rememberCoroutineScope()
+    TabRow(
+        selectedTabIndex = pagerState.currentPage,
+        indicator = { tabPositions ->
+            TabRowDefaults.Indicator(
+                Modifier.pagerTabIndicatorOffset(
+                    pagerState, tabPositions
+                )
+            )
+        }
     ) {
-        MarvelItemList(
-            items = comicsState,
-            onClick = onClick
-        )
+        formats.forEach {
+            Tab(
+                selected = it.ordinal == pagerState.currentPage,
+                onClick = { scope.launch { pagerState.animateScrollToPage(it.ordinal) } },
+                text = { Text(text = it.name) }
+            )
+        }
     }
 }
 
